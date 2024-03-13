b8ab769f9cb1f2a4a666df9ce183e88794334035
min <goyoumin2@gmail.com>
2024-03-14 05:40:00 +0900

    fix

--- src/main/java/com/afunproject/dawncraft/dungeon/block/entity/DungeonBlockEntities.java
+++ src/main/java/com/afunproject/dawncraft/dungeon/block/entity/DungeonBlockEntities.java
@@ -1,16 +1,9 @@
 package com.afunproject.dawncraft.dungeon.block.entity;
 
-import java.util.stream.Stream;
-
 import com.afunproject.dawncraft.Constants;
 import com.afunproject.dawncraft.dungeon.block.DungeonBlocks;
 
-import net.minecraft.Util;
-import net.minecraft.util.datafix.fixes.References;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.BlockEntityType.BlockEntitySupplier;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -19,20 +12,12 @@ public class DungeonBlockEntities {
 
 	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, Constants.MODID);
 
-	public static final RegistryObject<BlockEntityType<DungeonDoorBlockEntity>> DUNGEON_DOOR = register("dungeon_door", DungeonDoorBlockEntity::new,
-			DungeonBlocks.FIRE_DOOR, DungeonBlocks.RUST_DOOR, DungeonBlocks.SAND_DOOR, DungeonBlocks.STONE_DOOR, DungeonBlocks.WOOD_DOOR);
-	public static final RegistryObject<BlockEntityType<ChestSpawnerBlockEntity>> CHEST_SPAWNER = register("chest_spawner",
-			ChestSpawnerBlockEntity::new, DungeonBlocks.CHEST_SPAWNER);
-	public static final RegistryObject<BlockEntityType<RedstoneTriggerBlockEntity>> REDSTONE_TRIGGER = register("redstone_trigger",
-			RedstoneTriggerBlockEntity::new, DungeonBlocks.REDSTONE_TRIGGER);
-	public static final RegistryObject<BlockEntityType<RedstoneActivatorBlockEntity>> REDSTONE_ACTIVATOR = register("redstone_activator",
-			RedstoneActivatorBlockEntity::new, DungeonBlocks.REDSTONE_ACTIVATOR);
-
-	@SafeVarargs
-	private static <T extends BlockEntity> RegistryObject<BlockEntityType<T>> register(String name, BlockEntitySupplier<T> supplier, RegistryObject<Block>... blocks) {
-		return BLOCK_ENTITIES.register(name, () ->
-		BlockEntityType.Builder.of(supplier, (Block[]) Stream.of(blocks).map(RegistryObject::get).toArray())
-		.build(Util.fetchChoiceType(References.BLOCK_ENTITY, Constants.locStr(name))));
-	}
-
+	public static final RegistryObject<BlockEntityType<DungeonDoorBlockEntity>> DUNGEON_DOOR = BLOCK_ENTITIES.register("dungeon_door",  () -> BlockEntityType.Builder.of(DungeonDoorBlockEntity::new,
+			DungeonBlocks.FIRE_DOOR.get(), DungeonBlocks.RUST_DOOR.get(), DungeonBlocks.SAND_DOOR.get(), DungeonBlocks.STONE_DOOR.get(), DungeonBlocks.WOOD_DOOR.get()).build(null));
+	public static final RegistryObject<BlockEntityType<ChestSpawnerBlockEntity>> CHEST_SPAWNER = BLOCK_ENTITIES.register("chest_spawner", () -> BlockEntityType.Builder.of(
+			ChestSpawnerBlockEntity::new, DungeonBlocks.CHEST_SPAWNER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<RedstoneTriggerBlockEntity>> REDSTONE_TRIGGER = BLOCK_ENTITIES.register("redstone_trigger", () -> BlockEntityType.Builder.of(
+			RedstoneTriggerBlockEntity::new, DungeonBlocks.REDSTONE_TRIGGER.get()).build(null));
+	public static final RegistryObject<BlockEntityType<RedstoneActivatorBlockEntity>> REDSTONE_ACTIVATOR = BLOCK_ENTITIES.register("redstone_activator", () -> BlockEntityType.Builder.of(
+			RedstoneActivatorBlockEntity::new, DungeonBlocks.REDSTONE_ACTIVATOR.get()).build(null));
 }
